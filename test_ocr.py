9c5c72cde1099fb28063546c9d34e954add11fb2
SANGJUN KIM <weareyoyo12@naver.com>
2021-06-14 15:41:17 +0900

    불필요한 주석 제거

--- test_ocr.py
+++ test_ocr.py
@@ -1,137 +1,59 @@
 import easyocr
+import json
 from difflib import SequenceMatcher
-from jamo import h2j, j2hcj
+from collections import OrderedDict
+
+def recognition_rate_check(extracted_detail0_list):
+    #인식된 문장을 공백 단위로 슬라이싱
+    slicedResult = "".join(extracted_detail0_list)
+    slicedResult = slicedResult.split()
+    print(slicedResult)
+
+    #원본을 공백 단위로 슬라이싱, 가운뎃점 제거"
+    with open("OriginalTXT.txt", "r") as tf:
+        originalText = []
+        for line in tf:
+            originalText.append(line)
+
+    dotConvOriginal = []
+    for i in originalText:
+        dotFind = i.replace("·", " ")
+        dotConvOriginal.append(dotFind)
+
+    slicedOriginal = "".join(dotConvOriginal)
+    slicedOriginal = slicedOriginal.split()
+    print(slicedOriginal)
+
+    #OCR결과물과 원본의 예상 오차 비율(공백 무시)
+    originalString = "".join(slicedOriginal)
+    extractedString = "".join(slicedResult)
+
+    print("OCR결과물과 원본의 예상 오차 비율(공백 무시): ", SequenceMatcher(lambda x: x == " ", originalString, extractedString).ratio())
+
+
+def json_file_creation(extracted_data_list):
+    file_data = OrderedDict()
+    file_data['page'] = 1
+    item_list = []
+    for id, (area, text) in enumerate(extracted_data_list):
+        item = {}
+        item['id'] = id + 1
+        item['type'] = 'text'
+        item['area'] = area
+        item['content'] = text
+        item_list.append(item)
+    file_data['object'] = item_list
+    with open('page_.json', 'w', encoding="utf-8") as make_file:
+        json.dump(file_data, make_file, ensure_ascii=False, indent="\t")
 
 
 IMAGE_PATH = 'OCRtestImage.png'
-print("\nOCR로 인식된 내용리스트")
 reader = easyocr.Reader(['ko', 'en'], gpu=False)  # need to run only once to load model into memory
-result = reader.readtext(IMAGE_PATH, detail=0, paragraph=True)
-print(result)
+result = reader.readtext(IMAGE_PATH, paragraph=True)
+result_for_test = reader.readtext(IMAGE_PATH, detail=0, paragraph=True)
+
+recognition_rate_check(result_for_test)
+json_file_creation(result)
 
 
-print("\n인식된 문장을 공백 단위로 슬라이싱")
-slicedResult = "".join(result)
-slicedResult = slicedResult.split()
-print(slicedResult)
-
-print("\n원본을 공백 단위로 슬라이싱")
-with open("OriginalTXT.txt", "r") as tf:
-    originalText = []
-    for line in tf:
-        originalText.append(line)
-
-dotConvOriginal =[]
-for i in originalText:
-    dotFind = i.replace("·", " ")
-    dotConvOriginal.append(dotFind)
-
-slicedOriginal = "".join(dotConvOriginal)
-slicedOriginal = slicedOriginal.split()
-print(slicedOriginal)
-
-print("\nOCR결과물 중 원본과의 불일치 요소 출력")
-differenceOCR = []
-differenceOriginal = []
-for i in slicedResult:
-    if i not in slicedOriginal:
-        differenceOCR.append(i)
-for i in slicedOriginal:
-    if i not in slicedResult:
-        differenceOriginal.append(i)
-print("원본", differenceOriginal)
-print("추출본", differenceOCR)
-
-
-print("\nOCR결과물과 원본의 예상 오차 비율(공백 무시)")
-originalString = "".join(slicedOriginal)
-extractedString = "".join(slicedResult)
-
-print(SequenceMatcher(lambda  x: x==" ", originalString, extractedString).ratio())
-
-print("\nOCR결과물과 원본의 예상 오차 비율(자모 분리 후)")
-jamoOriginalString = j2hcj(h2j(originalString))
-jamoExtractedString = j2hcj(h2j(extractedString))
-
-print("자모 분리 원본", jamoOriginalString)
-print("자모 분리 추출본", jamoExtractedString)
-print('자모 분리 이후 SequenceMatcher: ', SequenceMatcher(lambda  x: x==" ", jamoOriginalString, jamoExtractedString).ratio())
-
-
-"""
-print("\nOCR결과물과 원본의 예상 오차 비율")
-print("원본의 전체 음절 수: ", len(slicedOriginal))
-print("추출본의 전체 음절 수: ", len(slicedResult))
-
-originalString = "".join(originalText)
-incorrectString = "".join(differenceOriginal)
-print('원본 전체 어절 수: ', len(originalString))
-print('추출본과 불일치한 원본 전체 어절 수: ', len(incorrectString))
-
-matchRate = ((len(originalString)-len(incorrectString))/len(originalString))*100
-
-print("어절 단위 원본과 추출본의 일치율: ", matchRate, "%")
-"""
-
-"""
-print("\nOCR 결과물 띄어쓰기 제거")
-RemoveBlank = [i.replace(' ', '') for i in result]
-print(RemoveBlank)
-
-print("\n띄어쓰기 제거된 원본")
-with open("OriginalTXT.txt", "r") as tf:
-    originalText = []
-    for line in tf:
-        originalText.append(line)
-    originalText = [i.replace(' ', '') for i in originalText]
-    originalText = [i.replace('\n', '') for i in originalText]
-print(originalText)
-
-print("\n OCR결과물과 원본의 비교")
-ocrString = np.array(RemoveBlank)
-print(ocrString)
-
-originString = np.array(originalText)
-print(originString)
-
-print(ocrString == originString)
-"""
-
-
-"""
-from difflib import SequenceMatcher
-import numpy as np
-import cv2
-import random
-import matplotlib.pyplot as plt
-from PIL import ImageFont, ImageDraw, Image
-
-img = cv2.imread(IMAGE_PATH)
-
-img = Image.fromarray(img)
-font = ImageFont.truetype("fonts/D2Coding-Ver1.3.2-20180524.ttc", 8)
-draw = ImageDraw.Draw(img)
-
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(255, 3), dtype="uint8")
-
-for i in result:
-    x = i[0][0][0]
-    y = i[0][0][1]
-    w = i[0][1][0] - i[0][0][0]
-    h = i[0][2][1] - i[0][1][1]
-
-    color_idx = random.randint(0, 255)
-    color = [int(c) for c in COLORS[color_idx]]
-
-    #    cv2.putText(img, str(i[1]), (int((x + x + w) / 2) , y-2), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
-    #    img = cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
-    draw.rectangle(((x, y), (x + w, y + h)), outline=tuple(color), width=1)
-    draw.text((int((x + x + w) / 2), y - 3), str(i[1]), font=font, fill=tuple(color), )
-
-plt.imshow(img)
-plt.show()
-# cv2.imshow("test",img)
-# cv2.waitKey(0)
-"""""
 
