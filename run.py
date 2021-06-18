ecbe9a5a51bdcbd9d69370a5188e6550494a1b3b
SANGJUN KIM <weareyoyo12@naver.com>
2021-06-18 11:22:00 +0900

    README.md 수정

--- run.py
+++ run.py
@@ -30,7 +30,6 @@ def recognition_rate_check(extracted_detail0_list):
 
     print("OCR결과물과 원본의 예상 오차 비율(공백 무시): ", SequenceMatcher(lambda x: x == " ", originalString, extractedString).ratio())
 
-
 def create_json_file(extracted_data_list):
     file_data = OrderedDict()
     file_data['page'] = 1
@@ -48,7 +47,7 @@ def create_json_file(extracted_data_list):
 
 
 IMAGE_PATH = 'OCRtestImage.png'
-reader = easyocr.Reader(['ko', 'en'], gpu=False)  # need to run only once to load model into memory
+reader = easyocr.Reader(['ko', 'en'], gpu=False)
 result = reader.readtext(IMAGE_PATH, paragraph=True)
 result_for_test = reader.readtext(IMAGE_PATH, detail=0, paragraph=True)
 
