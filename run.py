474b82a03f2e1c9f6d93f7adc1feed55e89766b1
SANGJUN KIM <weareyoyo12@naver.com>
2021-07-02 17:12:35 +0900

    일치도 검사 기준 세분화

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import json
 import time
 from difflib import SequenceMatcher
 from collections import OrderedDict
+from jamo import h2j, j2hcj
 
 def recognition_rate_check(extracted_detail0_list):
     #인식된 문장을 공백 단위로 슬라이싱
@@ -29,7 +30,80 @@ def recognition_rate_check(extracted_detail0_list):
     originalString = "".join(slicedOriginal)
     extractedString = "".join(slicedResult)
 
-    print("OCR결과물과 원본의 예상 오차 비율(공백 무시): ", SequenceMatcher(lambda x: x == " ", originalString, extractedString).ratio())
+    print("\nSequenceMatcher를 통한 음절단위 일치도: ", SequenceMatcher(lambda x: x == " ", originalString, extractedString).ratio())
+
+    print("\n어절 단위 OCR결과물 중 원본과의 불일치 요소 출력")
+    differenceOCR = []
+    differenceOriginal = []
+    for i in slicedResult:
+        if i not in slicedOriginal:
+            differenceOCR.append(i)
+    for i in slicedOriginal:
+        if i not in slicedResult:
+            differenceOriginal.append(i)
+    print("원본", differenceOriginal)
+    print("추출본", differenceOCR)
+
+    print("원본의 전체 어절 수: ", len(slicedOriginal))
+    print("추출본의 전체 어절 수: ", len(slicedResult))
+
+    print("\n어절 단위 OCR결과물 중 원본과의 불일치 요소 출력")
+    syllableOriginalString = "".join(differenceOriginal)
+    syllableExtractedString = "".join(differenceOCR)
+
+    syllableDifferenceOCR=[]
+    syllableDifferenceOriginal=[]
+    syllableCorrectOCR = []
+    for i in syllableOriginalString:
+        if i not in syllableExtractedString:
+            syllableDifferenceOriginal.append(i)
+    for i in syllableExtractedString:
+        if i not in syllableOriginalString:
+            syllableDifferenceOCR.append(i)
+    for i in syllableOriginalString:
+        if i in syllableExtractedString:
+            syllableCorrectOCR.append(i)
+    print('음절 단위 일치요소: ', syllableCorrectOCR)
+    print('\n원본 전체 음절 수(originalString): ', len(originalString))
+    print('추출본 전체 음절 수(extractedString):', len(extractedString))
+    print('\n원본 음절 단위 불일치 요소:', syllableDifferenceOriginal)
+    print('추출본 음절 단위 불일치 요소', syllableDifferenceOCR)
+
+    syllableRecall = (len(syllableCorrectOCR)/len(syllableOriginalString)) * 100
+    syllablePrecision = (len(syllableCorrectOCR)/len(syllableExtractedString)) * 100
+
+    print("\n음절 단위 recall: ", syllableRecall, "%")
+    print("음절 단위 precision: ", syllablePrecision, "%\n")
+
+    print("\n음소 단위 OCR결과물 중 원본과의 불일치 요소 출력")
+    phonemeOriginalString = j2hcj(h2j(syllableOriginalString))
+    phonemeExtractedString = j2hcj(h2j(syllableExtractedString))
+
+    phonemeDifferenceOCR = []
+    phonemeDifferenceOriginal = []
+    phonemeCorrectOCR = []
+    for i in phonemeOriginalString:
+        if i not in phonemeExtractedString:
+            phonemeDifferenceOriginal.append(i)
+    for i in phonemeExtractedString:
+        if i not in phonemeOriginalString:
+            phonemeDifferenceOCR.append(i)
+    for i in phonemeOriginalString:
+        if i in phonemeExtractedString:
+            phonemeCorrectOCR.append(i)
+
+    print('음소 단위 일치요소: ', phonemeCorrectOCR)
+    print('원본 음소 단위 불일치 요소:', phonemeDifferenceOriginal)
+    print('추출본 음소 단위 불일치 요소', phonemeDifferenceOCR)
+
+    phonemeRecall = (len(phonemeCorrectOCR) / len(phonemeOriginalString)) * 100
+    phonemePrecision = (len(phonemeCorrectOCR) / len(phonemeExtractedString)) * 100
+
+    print("\n음소 단위 recall: ", phonemeRecall, "%")
+    print("음소 단위 precision: ", phonemePrecision, "%")
+
+
+
 
 def create_json_file(extracted_data_list):
     file_data = OrderedDict()
